557bffa11e86ebc2b0c48300da14d10428747f38
Jeff Ogah <jeff.ogah@gmail.com>
2020-09-08 06:15:23 +0100

    Typo

--- src/components/NominationList.js
+++ src/components/NominationList.js
@@ -19,7 +19,7 @@ const NominationList = (props) => {
     <>
       <strong>Nominations</strong>
       {nominationDisplay}
-  {props.nominations.length === 5 ? <strong>You have reached the maximum limit (5) gitfor nominations.</strong> : ""}
+  {props.nominations.length === 5 ? <strong>You have reached the maximum limit (5) for nominations.</strong> : ""}
     </>
   );
 };
